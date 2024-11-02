0320d8af8cf8c4dde7c1a9438317cbdd03777058
Car134 <carmelymely@gmail.com>
2024-11-02 18:38:53 +0200

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,37 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+package rechw;
+
+public class Main {
+
+	public static void main(String[] args) {
+		public int Positive(int j, int [] a ) {
+			int count = 0;
+			if(a[j] = 0)
+				return count;
+			if(a[i]>0) {
+				count++;
+			}
+			return Positive(j--, a);
+						
+		}
+		public int location(int n, int [] a, int i) {
+			if(i>a.length) {
+				return -1;
+			}
+			if(a[i] = n) {
+				return i;
+			}
+			return location(n, a, i++);
+			
+		}
+		public boolean uprising(int [] a, int i, int n) {
+			if(a[i]<n) {
+				return false
+			}
+			if (i>a.length) {
+				return true;
+			}
+			return uprising(a, i++, n+=a[i]);
+		}
+	}
+
 }
